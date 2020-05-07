495763a26788c3be5b02cb09d88b14ee6e070da8
Emma Khachatryan <xachatryan.emma@list.ru>
2020-05-07 14:10:10 +0400

    add Items

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -11,9 +11,9 @@ import java.util.List;
 import java.util.Map;
 
 public class FileUtil {
-    private static final String USER_PATH = "C:\\Users\\Aka\\IdeaProjects\\Items\\src\\util\\user.txt";
-    private static Object User;
+    private static final String ITEM_PATH = "C:\\Users\\Aka\\IdeaProjects\\Items\\src\\util\\item.txt";
 
+    private static final String USER_PATH = "C:\\Users\\Aka\\IdeaProjects\\Items\\src\\util\\user.txt";
 
     public static void serializeUserMap(Map<String, User> userMap) throws IOException, ClassNotFoundException {
 
@@ -26,49 +26,45 @@ public class FileUtil {
     }
 
     public static Map<String, User> deSerializeUserMap() throws IOException, ClassNotFoundException {
-      if(USER_PATH.length()!=0) {
+        if (USER_PATH.length() != 0) {
 
-          try {
-              ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(USER_PATH));
-              Object deserrialization = objectInputStream.readObject();
-              Map<String, User> userMap = (Map<String, model.User>) deserrialization;
-              objectInputStream.close();
-              return userMap;
-          }catch (EOFException e){
-              e.getMessage();
-          }
-      }
-        Map<String,User> userMap = new HashMap<>();
-       return userMap;
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(USER_PATH));
+                Object deserrialization = objectInputStream.readObject();
+                Map<String, User> userMap = (Map<String, model.User>) deserrialization;
+                objectInputStream.close();
+                return userMap;
+            } catch (EOFException e) {
+                e.getMessage();
+            }
+        }
+        Map<String, User> userMap = new HashMap<>();
+        return userMap;
     }
 
-    private static final String ITEM_PATH =
-            "C:\\Users\\Aka\\IdeaProjects\\Items\\src\\util\\item.txt";
 
     public static void serializeItem(List<Item> items) throws IOException {
 
         ObjectOutputStream objectOutputStream =
                 new ObjectOutputStream(new FileOutputStream(ITEM_PATH));
-
         objectOutputStream.writeObject(items);
         objectOutputStream.close();
 
     }
 
     public static List<Item> deSerializeItem() throws IOException, ClassNotFoundException {
+        if (ITEM_PATH.length() != 0) {
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(ITEM_PATH));
 
-       if (ITEM_PATH.length()!=0) {
-           try {
-               ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(ITEM_PATH));
-
-               Object deserialization = objectInputStream.readObject();
-               List<Item> items = (List<Item>) deserialization;
-               objectInputStream.close();
-               return items;
-           }catch (EOFException e){
-               e.getMessage();
-           }
-       }
+                Object deserialization = objectInputStream.readObject();
+                List<Item> items = (List<Item>) deserialization;
+                objectInputStream.close();
+                return items;
+            } catch (EOFException e) {
+                e.getMessage();
+            }
+        }
         List<Item> items = new ArrayList<>();
         return items;
 
